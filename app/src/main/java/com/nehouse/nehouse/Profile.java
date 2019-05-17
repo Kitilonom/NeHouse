d2459a7cfa6f36ff1361bf29cfa9120dc4445054
Alice105973 <kukushkinako13081999@gmail.com>
2019-05-17 13:52:12 +0300

    Once Again

--- app/src/main/java/com/nehouse/nehouse/Profile.java
+++ app/src/main/java/com/nehouse/nehouse/Profile.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 public class Profile extends Activity {
 
+    public static boolean logon = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,10 @@ public class Profile extends Activity {
     }
 
     public void ProfileConfirm (View view) {
+        if (!logon) {           //если вызвана из settings
+            logon = true;       //возвращаем флаг в предыдущее состояние
+            finish();           //завершаем активность
+        }
         Intent intent = new Intent(Profile.this, MainActivity.class);
         startActivity(intent);
     }
