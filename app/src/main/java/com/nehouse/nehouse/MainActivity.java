d922e5df3840ee636deb16ffe813fc4274c01ac0
Alice105973 <kukushkinako13081999@gmail.com>
2019-05-17 13:47:01 +0300

    Merge Errors

--- app/src/main/java/com/nehouse/nehouse/MainActivity.java
+++ app/src/main/java/com/nehouse/nehouse/MainActivity.java
@@ -11,12 +11,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		Intent intent = new Intent(MainActivity.this, WelcomePage.class);
-        startActivity(intent);
     }
 
     public void MainActivityCalendar (View view) {
         Intent intent = new Intent(MainActivity.this, Calendar.class);
         startActivity(intent);
     }
+
+    public void MainActivityBudget (View view) {
+        Intent intent = new Intent(MainActivity.this, Budget.class);
+        startActivity(intent);
+    }
+
+    public void MainActivityNotifications (View view) {
+        Intent intent = new Intent(MainActivity.this, Notifications.class);
+        startActivity(intent);
+    }
+
+    public void MainActivityShoppingList (View view) {
+        Intent intent = new Intent(MainActivity.this, ShoppingList.class);
+        startActivity(intent);
+    }
+
+    public void MainActivityContacts (View view) {
+        Intent intent = new Intent(MainActivity.this, Contacts.class);
+        startActivity(intent);
+    }
+
+    public void MainActivityWishlist (View view) {
+        Intent intent = new Intent(MainActivity.this, Wishes.class);
+        startActivity(intent);
+    }
+
+    public void MainActivitySettings (View view) {
+        Intent intent = new Intent(MainActivity.this, Settings.class);
+        startActivity(intent);
+    }
+
 }
