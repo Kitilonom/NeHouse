46a5e834b595d2537590847ed3d8fcda55a4276b
381706-1Kukushkina <39962007+Alice105973@users.noreply.github.com>
2019-05-21 00:48:51 +0300

    Update Settings.java

--- app/src/main/java/com/nehouse/nehouse/Settings.java
+++ app/src/main/java/com/nehouse/nehouse/Settings.java
@@ -2,9 +2,19 @@ package com.nehouse.nehouse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.nehouse.nehouse.Model.User;
 
 public class Settings extends AppCompatActivity {
 
@@ -12,20 +22,31 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-        int ID = 0; // Вот тут надо вытащить ID из базы данных
+
+        String id = MainActivity.currentUser.getUid(); // Вот тут надо вытащить ID из базы данных
         TextView userID = (TextView)this.findViewById(R.id.UserID); //переменная userID хранит TextView с id == "UserID"
-        userID.setText("UserID: "+ ID);
+        TextView name = (TextView)this.findViewById(R.id.AccName);
+        TextView bday = (TextView)this.findViewById(R.id.AccDateOfBirth);
+        TextView email = (TextView)this.findViewById(R.id.AccProfileEmail);
+        TextView phone = (TextView)this.findViewById(R.id.AccProfilePhone);
+
+        userID.setText("UserID: " + id);
+        name.setText("Name: " + MainActivity.user.getName());
+        bday.setText("DateOfBirth: " + MainActivity.user.getBday());
+        email.setText("Email: " + MainActivity.user.getEmail());
+        phone.setText("Phone: " + MainActivity.user.getPhone());
     }
 
-    public void SettingsProfile (View view) {
+    public void SettingsEditProfile (View view) {
         Intent intent = new Intent(Settings.this, Profile.class);
         Profile.logon = false;
         startActivity(intent);
     }
 
-    public void SettingsPassword (View view) {
-        Intent intent = new Intent(Settings.this, PasswordChanging.class);
-        Profile.logon = false;
+    public void SettingsLogOut (View view) {
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(Settings.this, WelcomePage.class);
         startActivity(intent);
     }
 
